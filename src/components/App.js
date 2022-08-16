559817cb83368c554128b498e36c822004e74159
SARTHAK  JAIN <63348066+svsarthakjain@users.noreply.github.com>
2022-08-16 20:49:32 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,15 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 const App = () => {
   return (
-    <div id="main"></div>
-  )
-}
-
+    <div id="main">
+      <h1 data-ns-test="project-name">Name</h1>
+      <h1 data-ns-test="project-description">
+        This is First React js Assingment
+      </h1>
+    </div>
+  );
+};
 
 export default App;
