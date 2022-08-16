d2480d97f389a5d2b63b23af0e3ce3d9c0bbc41c
SARTHAK  JAIN <63348066+svsarthakjain@users.noreply.github.com>
2022-08-16 21:01:46 +0530

    Add files via upload

---
+++ src/components/Description.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Description() {
+  return (
+    <h1 data-ns-test="project-description">
+      This is First React js Assingment
+    </h1>
+  );
+}
---
+++ src/components/Heading.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Heading() {
+  return <h1 data-ns-test="project-name">Name</h1>;
+}
