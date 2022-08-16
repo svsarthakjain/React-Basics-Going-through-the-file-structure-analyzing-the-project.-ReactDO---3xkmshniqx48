667a9be4ce627a61c96ce9eec65602447f9d182d
SARTHAK  JAIN <63348066+svsarthakjain@users.noreply.github.com>
2022-08-16 20:52:57 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,7 @@ import "../styles/App.css";
 const App = () => {
   return (
     <div id="main">
-      <h1 data-ns-test="project-name">Name</h1>
+      <h1 data-ns-test="project-name">React Basics, Going through the file structure & analyzing the project., ReactDO ... - In Class - Rendering Multiple Components with React</h1>
       <h2 data-ns-test="project-description">
         This is First React js Assingment
       </h2>
