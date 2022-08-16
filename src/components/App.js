11baa590e4d5a978a15465b6fe68212783cd7b97
SARTHAK  JAIN <63348066+svsarthakjain@users.noreply.github.com>
2022-08-16 21:01:14 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,12 @@
 import React, { Component, useState } from "react";
 import "../styles/App.css";
-
+import Heading from "./components/Heading.js";
+import Description from "./components/Description.js";
 const App = () => {
   return (
     <div id="main">
-      <h1 data-ns-test="project-name">React Basics, Going through the file structure & analyzing the project., ReactDO ... - In Class - Rendering Multiple Components with React</h1>
-      <h2 data-ns-test="project-description">
-        This is First React js Assingment
-      </h2>
+      <Heading />
+      <Description />
     </div>
   );
 };
